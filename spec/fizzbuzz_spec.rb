77d8625a1e43fb39e474007a9463ed1fe8d0e601
Ben Conway <bendavidconway@me.com>
2014-10-27 17:06:48 +0000

    Loading files

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -8,7 +8,7 @@ describe 'fizzbuzz' do
 	    expect(is_divisible_by_three?(3)).to be true
 			end
 
-			it 'is divisible by five' do
+			it 'is divisible by five ' do
 			expect(is_divisible_by_five?(5)).to be true
 			end
 
